935babf9a0e12a75842fca08e7c649b602119330
mkrishnakumar <mkrishnakumar454@gmail.com>
2025-06-26 21:18:59 +0530

    this is my changes

--- Corejava/src/com/test/corejava/Test.java
+++ Corejava/src/com/test/corejava/Test.java
@@ -3,6 +3,15 @@ package com.test.corejava;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello corejava");
+		Test t1=new Test();
+		t1.add();
+	}
+		public void add() {
+			int a=10;
+			int b=20;
+			int sum=a+b;
+			
+		}
 	}
 
-}
+
